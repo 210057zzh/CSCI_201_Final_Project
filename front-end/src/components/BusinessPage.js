3e4ef56766a49596341ac949e4d3e040bbc9f9df
210057zzh <davidz@usc.edu>
2021-04-17 20:06:32 -0700

    fix business page sign up

--- front-end/src/components/BusinessPage.js
+++ front-end/src/components/BusinessPage.js
@@ -6,7 +6,7 @@ import { useContext } from 'react';
 import { authContext } from './contexts/authContext'
 import BusinessPageHeader from './BusinessPageHeader'
 import BusinessPageBottom from './BusinessPageBottom.js'
-
+import Signup from './UserAuth/SignupPopup';
 
 function getBusinessInfo() {
     return (
@@ -32,7 +32,7 @@ function outputBusinessPage() {
     return (
         <div>
             <BusinessPageHeader name={business.name} hours={business.hours} category={business.category} rating={business.rating} reviewCount={business.reviewCount} priceLevel={business.priceLevel} />
-            <BusinessPageBottom  description={business.description} otherInfo={business.otherInfo} phone={business.phone} website={business.website} email={business.email} address={business.address}/>
+            <BusinessPageBottom description={business.description} otherInfo={business.otherInfo} phone={business.phone} website={business.website} email={business.email} address={business.address} />
         </div>)
 
 }
@@ -44,7 +44,8 @@ function BusinessPage(props) {
     return (
         <div className='home'>
             {authState.showLogin ? <Login /> : null}
-            {authState.showLogin ? <div className='darkened'></div> : null}
+            {authState.showSignup ? <Signup /> : null}
+            {(authState.showLogin || authState.showSignup) ? <div className='darkened'></div> : null}
             <Navbar />
             {outputBusinessPage()}
         </div>
