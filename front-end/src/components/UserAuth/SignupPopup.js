f948cedbf90e51941b30331a6d3254730d838ab9
210057zzh <davidz@usc.edu>
2021-04-18 18:11:00 -0700

    new sign up

--- front-end/src/components/UserAuth/SignupPopup.js
+++ front-end/src/components/UserAuth/SignupPopup.js
@@ -5,8 +5,9 @@ import axios from 'axios';
 import onClickOutside from 'react-onclickoutside'
 import { authContext } from '../contexts/authContext';
 import Error from './Error';
-
-
+import { validateSignUpForm } from './validate';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 /*
  Needs: 
@@ -20,9 +21,12 @@ function Signup(props) {
     const { authState, setAuthState } = useContext(authContext);
     const [email, setEmail] = useState();
     const [pass, setPass] = useState();
-    const [SecondPass, setSecondPass] = useState();
+    const [pwconfirm, setpwconfirm] = useState();
     const [err, setErr] = useState();
     const REST_API_CALL = 'http://localhost:8080/api/Signup'
+    const [emailerr, setEmailerr] = useState();
+    const [pwerr, setPwerr] = useState();
+    const [pwconfirmerr, setpwce] = useState();
 
     useEffect(() => {
         if (authState.signUpredirect === true) {
@@ -44,7 +48,21 @@ function Signup(props) {
         setPass(e.target.value);
     }
 
+    function updatepwconfirm(e) {
+        setpwconfirm(e.target.value);
+    }
+
     function submit(e) {
+        var payload = {
+            email: email,
+            password: pass,
+            pwconfirm: pwconfirm,
+        };
+        console.log(payload);
+        var result = validateSignUpForm(payload);
+        setEmailerr(result.errors.email);
+        setPwerr(result.errors.password);
+        setpwce(result.errors.pwconfirm);
         e.preventDefault();
         axios.post(REST_API_CALL, [email, pass]).then(async (resp) => {
             console.log(resp);
@@ -62,15 +80,52 @@ function Signup(props) {
     }
     return (
         <div className='Signup-popup'>
-            <div style={{ fontWeight: 'bold', fontSize: '36px', marginTop: '1em' }}>Sign up to Sprout</div>
-            <div style={{ marginTop: '4em' }}><GoogleLogin LoggedinStatus={authState.loggedIn}
+            <div style={{ fontWeight: 'bold', fontSize: '30px', marginTop: '1em' }}>Sign up to Sprout</div>
+            <div style={{ marginTop: '1em' }}><GoogleLogin LoggedinStatus={authState.loggedIn}
                 buttonText="Sign up with Google" signUporLogin={'Signup'} /> </div>
-            <hr style={{ width: '70%', marginTop: '4em' }}></hr>
-            <form className='Signup-form'>
-                <input type='text' placeholder='Email...' onChange={updateEmail} required />
-                <input type='password' placeholder='Password...' onChange={updatePass} required />
+            <hr style={{ width: '30%', marginTop: '3em' }}></hr>
+            <form className='Signup-form' onSubmit={submit}>
+                <TextField
+                    size='medium'
+                    placeholder='apple@orange.com....'
+                    margin='dense'
+                    name="email"
+                    label="email"
+                    onChange={updateEmail}
+                    error={emailerr}
+                    helperText={emailerr ? emailerr : null}
+                    required={true}
+                />
+                <br></br>
+                <TextField
+                    type='password'
+                    size='medium'
+                    placeholder='Password...'
+                    onChange={updatePass}
+                    margin='dense'
+                    name="pass"
+                    label="password"
+                    error={pwerr}
+                    helperText={pwerr ? pwerr : 'at least 8 characters'}
+                    required={true}
+                />
+                <br></br>
+                <TextField
+                    className='Signup-form input'
+                    type='password'
+                    size='medium'
+                    placeholder='confirm your pass...'
+                    onChange={updatepwconfirm}
+                    margin='dense'
+                    name='pwconfirm'
+                    label="confirm password"
+                    error={pwconfirmerr}
+                    helperText={pwconfirmerr ? pwconfirmerr : 'enter your password to confirm'}
+                    required={true}
+                />
                 {err ? <Error errorMsg={err}></Error> : null}
-                <input type="submit" className='Signup-btn' onClick={submit} align="center" />
+                <br></br>
+                <Button style={{ width: '20%', marginTop: '3em' }} className='Signup-btn' size="small" type="submit" variant="contained" color="">Submit</Button>
             </form>
         </div>
     );
