e83cd486f209434068f709ece1aba7bdc054be55
Nikhil Patel <napatel@usc.edu>
2021-04-24 14:09:54 -0700

    Adding review works

--- back-end/sprout/src/main/java/com/Sprout/Group/springboot/controller/InsertIntoController.java
+++ back-end/sprout/src/main/java/com/Sprout/Group/springboot/controller/InsertIntoController.java
@@ -20,6 +20,7 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,16 +43,18 @@ public class InsertIntoController {
 	public InsertIntoController(JdbcTemplate jdbcTemplate) {
 		this.jdbcTemplate = jdbcTemplate;
 	}
-	@GetMapping("/addReview")
+	@PostMapping("/addReview")
 	public void InsertBusiness(@RequestParam int userID, @RequestParam int businessID,@RequestParam String message,@RequestParam int rating) {
 		String url = dbAddress_nopass;
 		DriverManagerDataSource dataSource = new DriverManagerDataSource(url, "root", "root");
-		SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert(dataSource).withTableName("Review");
+		SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert(dataSource).withTableName("Reviews");
 		Map<String, Object> parameters = new HashMap<String, Object>();
 		parameters.put("businessID", businessID);
+		parameters.put("userID", userID);
 		parameters.put("message", message);
 		parameters.put("rating", rating);
-		parameters.put("time", getCurrentDate());
+		//parameters.put("time", getCurrentDate());
+		//TODO for some reason it just crashes if you try and insert the time
 		int return_value = simpleJdbcInsert.execute(parameters);
 		
 		System.out.println("VALUE RETURNED IS :" + return_value);
