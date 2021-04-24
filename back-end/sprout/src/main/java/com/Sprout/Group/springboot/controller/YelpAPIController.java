d49bf0080b8097966a491948786aaa938ebbb56d
Nikhil Patel <napatel@usc.edu>
2021-04-24 13:12:31 -0700

    Changes to yelp API controller
    adds address parsing to backend

--- back-end/sprout/src/main/java/com/Sprout/Group/springboot/controller/YelpAPIController.java
+++ back-end/sprout/src/main/java/com/Sprout/Group/springboot/controller/YelpAPIController.java
@@ -29,22 +29,22 @@ public class YelpAPIController {
 	// address
 	// Else returns -1
 	@GetMapping("/yelpfill")
-	public String discoverBusinesses(@RequestParam String[] businessInfo) {
-		return yelpFill(businessInfo);
+	public String discoverBusinesses(@RequestParam String name, @RequestParam String address) {
+		return yelpFill(name, address);
 	}
 
-	private static String yelpFill(String[] businessInfo) {
+	private static String yelpFill(String name, String address) {
 		// Formats params for url
 		// TODO may have to do more replaces than just spaces
-		for (int i = 0; i < businessInfo.length; i++) {
-			businessInfo[i] = businessInfo[i].replace(" ", "%20");
-		}
+		address=address.replace(", ",",");
+        address=address.replace(" ", "%20");
 
-		final String name = businessInfo[0];
-		final String address1 = businessInfo[1];
-		final String city = businessInfo[2];
-		final String state = businessInfo[3];
-		final String country = businessInfo[4];
+        String address1 = address.substring(0, address.indexOf(","));
+        address=address.substring(address.indexOf(",")+1);
+        String city = address.substring(0, address.indexOf(","));
+        address=address.substring(address.indexOf(",")+1);
+        String state = address;
+        String country="US";
 
 		// Get the business id
 		String id = "";
