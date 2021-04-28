6c5f28eb38deaa1397c28f87e2533383a1e1903c
Nikhil Patel <napatel@usc.edu>
2021-04-27 20:38:37 -0700

    Deleted an unused line and changed 2 methods to posts

--- back-end/sprout/src/main/java/com/Sprout/Group/springboot/controller/FavoritesController.java
+++ back-end/sprout/src/main/java/com/Sprout/Group/springboot/controller/FavoritesController.java
@@ -13,6 +13,7 @@ import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,84 +21,86 @@ import org.springframework.web.bind.annotation.RestController;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import models.Business;
-
 @CrossOrigin(origins = origins)
 @RestController
 @RequestMapping("/api")
 public class FavoritesController {
-	
+
 	private final JdbcTemplate jdbcTemplate;
 
 	public FavoritesController(JdbcTemplate jdbcTemplate) {
 		this.jdbcTemplate = jdbcTemplate;
 	}
 
-	//Takes in the business id and user id and responds whether the business is a favorite
+	// Takes in the business id and user id and responds whether the business is a
+	// favorite
 	@GetMapping("/isFavorite")
 	public String isFavorite(@RequestParam int userID, @RequestParam int businessID) {
-		var favorites =  this.jdbcTemplate.queryForList("SELECT * FROM Favorites where userID="+userID+" and businessID="+businessID).stream()
-				.collect(Collectors.toList());
-				
-		if(favorites.size()>=1) {
+		var favorites = this.jdbcTemplate
+				.queryForList("SELECT * FROM Favorites where userID=" + userID + " and businessID=" + businessID)
+				.stream().collect(Collectors.toList());
+
+		if (favorites.size() >= 1) {
 			return "{\"isFavorite\":true}";
-		}else {
+		} else {
 			return "{\"isFavorite\":false}";
 		}
 	}
-	
-	//Takes in the business id and user id and adds the business to the favorites database
-	@GetMapping("/addFavorite")
-	public String addFavorite(@RequestParam int userID, @RequestParam int businessID) {		
+
+	// Takes in the business id and user id and adds the business to the favorites
+	// database
+	@PostMapping("/addFavorite")
+	public String addFavorite(@RequestParam int userID, @RequestParam int businessID) {
 		try {
 			String url = dbAddress_nopass;
 			DriverManagerDataSource dataSource = new DriverManagerDataSource(url, "root", "root");
 			SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert(dataSource).withTableName("Favorites");
 			Map<String, Object> parameters = new HashMap<String, Object>();
-			
+
 			parameters.put("userID", userID);
 			parameters.put("businessID", businessID);
-			
+
 			int return_value = simpleJdbcInsert.execute(parameters);
-			
-			if(return_value==0) {
+
+			if (return_value == 0) {
 				return "{\"success\":false}";
-			}else {
+			} else {
 				return "{\"success\":true}";
 			}
-		}catch(Exception e) {
+		} catch (Exception e) {
 			return "{\"success\":false}";
 		}
 	}
-	
-	//Takes in the business id and user id and removes the business from the favorites table
-	@GetMapping("/removeFavorite")
+
+	// Takes in the business id and user id and removes the business from the
+	// favorites table
+	@PostMapping("/removeFavorite")
 	public String removeFavorite(@RequestParam int userID, @RequestParam int businessID) {
 		try {
 			String deleteQuery = "delete from Favorites where userID = ? and businessId=?";
 			jdbcTemplate.update(deleteQuery, userID, businessID);
 			return "{\"success\":true}";
-		}catch(Exception e) {
+		} catch (Exception e) {
 			return "{\"success\":false}";
 		}
 	}
-	
-	//Takes in a user id and returns all the favorites's business info
+
+	// Takes in a user id and returns all the favorites's business info
 	@GetMapping("/getUserFavorites")
 	public String getUserFavorites(@RequestParam int userID) {
-		var favorites =  this.jdbcTemplate.queryForList("SELECT * FROM Favorites where userID="+userID).stream()
+		var favorites = this.jdbcTemplate.queryForList("SELECT * FROM Favorites where userID=" + userID).stream()
 				.collect(Collectors.toList());
-		
-		String retString="{[";
+
 		Gson gson = new GsonBuilder().create();
 		ArrayList<Map<String, Object>> businesses = new ArrayList<>();
-		
-		for(Map<String, Object> favorite: favorites) {
-			var business = this.jdbcTemplate.queryForList("Select * From Businesses where businessID="+favorite.get("businessID")).stream()
+
+		for (Map<String, Object> favorite : favorites) {
+			var business = this.jdbcTemplate
+					.queryForList("Select * From Businesses where businessID=" + favorite.get("businessID")).stream()
 					.collect(Collectors.toList());
 			businesses.add(business.get(0));
 		}
-		
+
 		return gson.toJson(businesses);
 	}
 }
